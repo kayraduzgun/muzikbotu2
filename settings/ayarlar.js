004dd427ae62fe3b916e2bb5ee8d787bbe1fccc4
kayraduzgun <abdurkadircalar@gmail.com>
2023-05-21 08:24:47 +0300

    muzikbotu2

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTEwNzM0Njk0MzMyNTI2MTkwNQ.GYTOkw.9Qcl-lmS4SYLFq4c8xtPSVHVXw_6238wqgIgC4",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "1090328401556029480", //sahip id :()
+}
